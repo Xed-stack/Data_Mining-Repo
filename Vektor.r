53dea79151855c41aa0afdd403868b2c2f88827e
Xedrik <julioxedrik@gmail.com>
2025-09-15 18:23:29 +0800

    Third Commit

---
+++ Vektor.r
@@ -0,0 +1,9 @@
+Viktor <- c(20, 55, 70)
+
+if (all(Viktor > 50)){
+    print("All values are above 50")
+} else if (any(Viktor > 50)){
+    print("Some are above 50")
+} else {
+    print("None are above 50")
+}
