4b1a880b0d3f3c54b7246faf6c3ed19ab02c98fc
Xedrik <julioxedrik@gmail.com>
2025-09-15 17:34:09 +0800

    Done

---
+++ Gradingsys.r
@@ -0,0 +1,12 @@
+input <- 72
+if (input >= 90 && input <= 100 ){
+    print("A")
+} else if (input >= 80 && input <= 89){
+    print("B")
+} else if (input >= 70 && input <= 79){
+    print("C")
+} else if (input >= 60 && input <= 69){
+    print("D")
+} else {
+    print("F")
+}
