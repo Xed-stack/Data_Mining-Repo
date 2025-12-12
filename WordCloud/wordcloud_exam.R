bb90d0d3b789796e42cefd641e1b7be226a106f7
Xedrik <julioxedrik@gmail.com>
2025-12-12 10:15:17 +0800

    WordCloud

---
+++ WordCloud/wordcloud_exam.R
@@ -0,0 +1,79 @@
+library(tm)
+library(SnowballC)
+library(wordcloud)
+library(RColorBrewer)
+
+#Part 2 
+
+feedback <- readLines("feedback.txt", encoding = "UTF-8", warn = FALSE)
+feedback <- paste(feedback, collapse=" ")
+
+feedback_corpus <- Corpus(VectorSource(feedback))
+
+#Pre-Process
+# Convert to lowercase
+feedback_clean <- tm_map(feedback_corpus, content_transformer(tolower))
+feedback_clean <- tm_map(feedback_clean, content_transformer(function(x) gsub("[^a-z]"," ",x)))
+# Remove numbers
+feedback_clean <- tm_map(feedback_clean, removeNumbers)
+
+# Remove punctuation
+feedback_clean <- tm_map(feedback_clean, removePunctuation)
+
+# Remove English stopwords
+feedback_clean <- tm_map(feedback_clean, removeWords, stopwords("english"))
+
+# Strip whitespace
+feedback_clean <- tm_map(feedback_clean, stripWhitespace)
+
+# Stem the words
+feedback_clean <- tm_map(feedback_clean, stemDocument)
+
+#create TDM
+tdm <- TermDocumentMatrix(feedback_clean)
+m <- as.matrix(tdm)
+#Convert to Freq. table
+word_freqs <- sort(rowSums(m), decreasing = TRUE)
+df <- data.frame(word = names(word_freqs), freq = word_freqs)
+
+#Display top 10 words
+print("Top 10 Most Frequent Words:")
+print(head(df, 10))
+
+
+#Part 3
+png("wordcloud_exam.png", width = 800, height=600)
+set.seed(1234)
+wordcloud(
+  words = df$word,
+  freq = df$freq,
+  min.freq = 2,
+  max.words = 1000,
+  random.order = FALSE,
+  rot.per = 0.4,
+  scale = c(3, 0.5),
+  use.r.layout=TRUE,
+  colors = brewer.pal(8,"Spectral")
+)
+dev.off()
+cat("Saved wordcloud_exam.png\n")
+
+
+#Part 4
+rare_words <- subset(df, freq == 1)
+
+png("wordcloud_rare.png", width = 800, height = 600)
+
+wordcloud(
+  words =rare_words$word,
+  freq = rare_words$freq,
+  min.freq = 1,
+  max.words = nrow(rare_words),
+  random.order = FALSE,
+  rot.per = 0.35,
+  scale = c(4, 0.5),
+  colors = brewer.pal(8,"Spectral")
+)
+dev.off()
+cat("Saved wordcloud_rare.png\n")
+
